d0f03e0fb4c42b6339409a112222fdc4b1dac1ca
ChristianSalazar07 <christian.morillo@aluno.senac.rio>
2026-03-30 20:44:51 -0300

    Comentários Adicionados ao Exercicio 02

--- Lista 01/exercicio_02.py
+++ Lista 01/exercicio_02.py
@@ -1,6 +1,9 @@
+#Entrada
 valor01 = float(input("Diga o primeiro valor:"))
 valor02 = float(input("Diga o segundo valor:"))
 valor03 = float(input("Diga o terceiro valor:"))
 valor04 = float(input("Diga o quarto valor:"))
+#Processamento
 media_ponderada = ((valor01 * 1) + (valor02 * 2) + (valor03 * 3) + (valor04 * 4))/10
+#Saída
 print(f"A média ponderada é:{media_ponderada:.2f8}")
