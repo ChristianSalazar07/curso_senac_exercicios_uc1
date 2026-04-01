9635b41dd93c25b06139b747152feeab749e70e1
ChristianSalazar07 <christian.morillo@aluno.senac.rio>
2026-04-01 18:27:58 -0300

    Versão Alternativa do Exercicio 05 Lista 03

---
+++ Lista 03/exercicio_05_v2.py
@@ -0,0 +1,10 @@
+primeiro_numero = 85
+ultimo_numero = 907
+numeros_pares = []
+soma = 0
+for i in range(primeiro_numero, ultimo_numero):
+    if i%2 ==0:
+        numeros_pares.append(i)
+        soma = soma + i
+print(f"{numeros_pares}\n"
+      +f"A soma dos números pares é {soma:.0f}")
